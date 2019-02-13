2fc6df6bc9c4e36529bd2123bb3b37849764ec79
Karsten Donnay <karsten.donnay@uni-konstanz.de>
2019-02-13 12:38:33 +0100

    Changes to make robust to empty integration

--- R/meltt_data.R
+++ R/meltt_data.R
@@ -28,7 +28,9 @@ meltt_data.meltt <- function(object,columns=NULL,return_all=FALSE){
     out = dd2[,columns] # only select requested columns
   }
   out = out[order(out$date,out$dataset,out$event),] # order by date, if tied by dataset, then event
-  row.names(out) = 1:nrow(out) # re-index rows
-  out$dataset = dat.names[out$dataset] # restore data names
+  if (nrow(out) > 0){
+    row.names(out) = 1:nrow(out) # re-index rows
+    out$dataset = dat.names[out$dataset] # restore data names
+  }
   return(out)
 }
